fc71c8a396c82c5e1d5f4a0028aaf4c913e90716
r0elcompute <rodgersmwangi20@gmail.com>
2026-09-06 00:09:49 +0300

    fix backend

--- src/main.py
+++ src/main.py
@@ -175,12 +175,9 @@ def sync_payload(payload: schemas.SyncPayload, db: Session = Depends(get_db)):
 @app.get("/health")
 def health():
     return {"status": "ok"}
-<<<<<<< HEAD
 
 
 frontend_dir = Path(__file__).resolve().parent.parent / "frontend"
 
 if frontend_dir.exists():
     app.mount("/", StaticFiles(directory=frontend_dir, html=True), name="frontend")
-=======
->>>>>>> d54828691dc5b035f654ac4f5c219a0eb5ca8260
